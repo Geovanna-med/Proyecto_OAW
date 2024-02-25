7c5f09ff793e1860c79a06b900c5879b43ded60c
BeckyZhuWu <zhuwub@gmail.com>
2024-02-25 15:33:41 -0600

    feat: base de datos agregada

--- Primera_Entrega/read_feed.php
+++ Primera_Entrega/read_feed.php
@@ -6,74 +6,74 @@ error_reporting(0);
 if (isset($_GET['urls'])) {
     // Obtener la URL del feed del parámetro GET
     $urls = $_GET['urls'];
-    $html = "";
+    $con = configDatabase();
     foreach ($urls as $feedUrl) {
-        $feed = new SimplePie();
-        $feed->set_feed_url($feedUrl);
-        $feed->enable_cache(false);
-        $feed->strip_attributes(array('img'));
-        $feed->init();
-        $feed->handle_content_type();
-       
-        foreach ($feed->get_items() as $item) {
-            
-            if($item != null && !empty($item->get_date()) && !empty($item->get_title()) && !empty($item->get_description()) && !empty($item->get_categories())){
-                $html .= '<tr>'; 
-                $html .= '<td>' . $item->get_date('j-F-Y g:i a') . '</td>';
-                $html .= '<td>' . $item->get_title() . '</td>';
-                $html .= '<td><a href="' . $item->get_permalink() . '"> '. $item->get_permalink() .'</td>';
-                $html .= '<td>' . $item->get_description() . '</td>';
-                $categories = $item->get_categories();
-                usort($categories, function($a, $b) {
-                    return strcasecmp($a->get_label(), $b->get_label());
-                });
-
-                $html .= '<td>';
-                foreach ($categories as $category) {
-                    $html .= "<p>" . $category->get_label() . "<p>";
-                }
-                $html .= '</td>';
-                $html .= '</tr>';
-            }else{
-                $html .= '<tr>';
-                if($item->get_date() != null){
-                    $html .= '<td>' . $item->get_date('j-F-Y g:i a') . '</td>';
-                }else{
-                    $html .= '<td>' . " " . '</td>';
-                }
-                if($item->get_title() != null){
-                    $html .= '<td>' . $item->get_title() . '</td>';
-                }else{
-                    $html .= '<td>' . " " . '</td>';
-                }
-                if($item->get_permalink() != null){
-                    $html .= '<td><a href="' . $item->get_permalink() . '"> '. $item->get_permalink() .'</td>';
-                }else{
-                    $html .= '<td>' . " " . '</td>';
-                }
-                if($item->get_description()!=null){
-                    $html .= '<td>' . $item->get_description() . '</td>';
-                }else{
-                    $html .= '<td>' . " " . '</td>';
-                }
-                if($item->get_categories()!=null){
-                    $categories = $item->get_categories();
-                    usort($categories, function($a, $b) {
-                        return strcasecmp($a->get_label(), $b->get_label());
-                    });
-
-                    $html .= '<td>';
-                    foreach ($categories as $category) {
-                        $html .= "<p>" . $category->get_label() . "<p>";
-                    }
-                    $html .= '</td>';
-                }else{
-                    $html .= '<td>' . " " . '</td>';
-                }
-            }
-        }
+        $feed = configFeed($feedUrl);
+        insertItemsToDB($feed, $con);
     }
+    $sql = "SELECT * FROM `news`;";
+    $results = $con->query($sql);
+    $html = createHTMLTable($results);
     echo $html;
 } else {
     echo "No se proporcionó la URL del feed.";
+}
+
+function configDatabase() {
+    $con = new mysqli('localhost', 'root', '');
+    $con->query("CREATE DATABASE IF NOT EXISTS rss_feed");
+    $con->select_db('rss_feed');
+    $sql = "CREATE TABLE IF NOT EXISTS  news (
+        date DATETIME,
+        title VARCHAR(255),
+        url VARCHAR(255),
+        description TEXT,
+        category VARCHAR(255)
+    )";
+    $con->query($sql);
+    $con->query("DELETE FROM `news`;");
+    return $con;
+}
+
+function configFeed($url) {
+    $feed = new SimplePie();
+    $feed->set_feed_url($url);
+    $feed->enable_cache(false);
+    $feed->strip_attributes(array('img'));
+    $feed->init();
+    $feed->handle_content_type();
+    return $feed;
+}
+
+function createHTMLTable($results) {
+    $html = "";
+    foreach( $results as $result) {
+        $html .= '<tr> <td>'. $result['date']. '</td>';
+        $html .= '<td>'. $result['title']. '</td>';
+        $html .= '<td><a href="'. $result['url']. '" target="_blank">'. $result['url']. '</a></td>';
+        $html .= '<td>'. $result['description']. '</td>';
+        $html .= '<td>'. $result['category']. '</td> </tr>';
+    }
+    return $html;
+}
+
+function insertItemsToDB($feed, $con) {
+    foreach ($feed->get_items() as $item) {
+        $date = $con->real_escape_string($item->get_date('Y-m-d H:i:s'));
+        $title = $con->real_escape_string($item->get_title());
+        $url = $con->real_escape_string($item->get_permalink());
+        $description = $con->real_escape_string($item->get_description());
+        $description = str_replace(array("\r", "\n"), ' ', $description);
+        
+        $category = '';
+    
+        foreach ($item->get_categories() as $cat) {
+            $category .= $con->real_escape_string($cat->get_label()) . ', ';
+        }
+    
+        $category = rtrim($category, ', ');
+    
+        $sql = "INSERT INTO news (date, title, url, description, category) VALUES ('$date', '$title', '$url', '$description', '$category')";
+        $con->query($sql);
+    }
 }
